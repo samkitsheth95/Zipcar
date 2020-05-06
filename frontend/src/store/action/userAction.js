37ad625b609de8b13c6f38d3b8119eb58e37dc50
varshitshah <varshitraju.shah@sjsu.edu>
2020-05-06 16:46:21 -0700

    Corrected Error Handling For Axios calls

--- frontend/src/store/action/userAction.js
+++ frontend/src/store/action/userAction.js
@@ -2,48 +2,46 @@
 import request from '../../helpers/communication';
 
 const userupdate = (e) => (dispatch) => {
-  request.sendPost('user/update', e.target).then(() => {
-    $('.modal').modal('hide');
-    dispatch({ type: 'USERUPDATE' });
-  }, () => {
-    // dispatch({ type: 'ERROR' });
-  });
+  request.sendPost('user/update', e.target)
+    .then(() => {
+      $('.modal').modal('hide');
+      dispatch({ type: 'USERUPDATE' });
+    });
 };
 
 const getuser = () => (dispatch) => {
-  request.get('user/getuser').then((res) => {
-    dispatch({ type: 'GETUSER', payload: res.data });
-  }, () => {
-    // dispatch({ type: 'ERROR' });
-  });
+  request.get('user/getuser')
+    .then((res) => {
+      dispatch({ type: 'GETUSER', payload: res.data });
+    });
 };
 
 const search = (e) => (dispatch) => {
-  request.sendPost('user/search', e.target).then((res) => {
-    dispatch({ type: 'SEARCHRES', payload: res.data });
-  }, () => {
-    // dispatch({ type: 'ERROR' });
-  });
+  request.sendPost('user/search', e.target)
+    .then((res) => {
+      dispatch({ type: 'SEARCHRES', payload: res.data });
+    });
 };
 
 const book = (ev, history) => (dispatch) => {
   ev.preventDefault();
   const { target } = ev;
-  request.sendPost('user/book', target).then((res) => {
-    console.log(res.data);
-    target.reset();
-    $('.modal').modal('hide');
-    history.push('/booking');
-  });
+  request.sendPost('user/book', target)
+    .then((res) => {
+      console.log(res.data);
+      target.reset();
+      $('.modal').modal('hide');
+      history.push('/booking');
+    })
+    .catch((error) => { console.log(error.response.data.error); });
 };
 
 const getbooking = () => (dispatch) => {
-  request.get('user/getbooking').then((res) => {
-    //console.log(res.data);
-    dispatch({ type: 'GETBOOKING', payload: res.data });
-  }, () => {
-    // dispatch({ type: 'ERROR' });
-  });
+  request.get('user/getbooking')
+    .then((res) => {
+    // console.log(res.data);
+      dispatch({ type: 'GETBOOKING', payload: res.data });
+    });
 };
 
 export default {
